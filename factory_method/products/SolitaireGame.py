698c90bfac5f3c349640a1c1a1e4cdd80e7c4449
Lorena <75336847+nalama1@users.noreply.github.com>
2025-10-11 14:54:18 -0500

    cambios dentro de solitaire-game 1

--- factory_method/products/SolitaireGame.py
+++ factory_method/products/SolitaireGame.py
@@ -5,20 +5,38 @@ from ..audit.Logger import Logger
 
 
 class SolitaireGame(Game):
-    def __init__(self, player):
+    """ Representa una implementación concreta de un juego de Solitario.
+        Hereda de la clase abstracta/interfaz Game y registra eventos.
+    """
+    def __init__(self, player:str):
         self.player = player
         self.name_file = __file__
         self.name_base = os.path.basename(self.name_file)
+        self.start_time: datetime.datetime = datetime.datetime.now()
+
+        # Atributos de estado
+        self.score: int = 0
+        self.status = "No iniciado"
+
+        # Inicialización de Logger
+        self._logger = Logger()
+
+        # Registro inicial
+        self._logger.log(f"Game started by {self.player} in {self.name_base} at {self.start_time}")
+
+
+    def start(self) -> None:
+        """ Inicia el juego, actualiza el estado y registra el evento"""
+        self.status = "En curso"
         self.start_time = datetime.datetime.now()
-        self.score = 0
-        self.status = "In progress"
 
-        # Record in the log
-        logger = Logger()
-        logger.log(f"Game started by {self.player} in {self.name_base} at {self.start_time}")
+        print("-" * 40)
+        print(f" El juego de Solitario ha comenzado")
+        print(f"Jugador: {self.player}")
+        print("-" * 40)
+
+        self._logger.log(
+            f"Juego de Solitario INICIADO por {self.player}. Tiempo: {self.start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 
-    def start(self):
-        print(f"[{self.name_base}] Player {self.player} starts Solitaire card game")
-
-    """ Concrete product: SolitaireGame"""
+# Concrete product: SolitaireGame
