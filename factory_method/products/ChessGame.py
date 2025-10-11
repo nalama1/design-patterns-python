350e335adb36d33cf688b80eba110bd46b6079eb
Lorena <75336847+nalama1@users.noreply.github.com>
2025-10-10 21:36:59 -0500

    cambios en chess game, se agregan constantes

--- factory_method/products/ChessGame.py
+++ factory_method/products/ChessGame.py
@@ -2,6 +2,14 @@ import os
 from .Game import Game
 
 class ChessGame(Game):
+    """
+    Representa una implementación concreta de un juego de Ajedrez.
+    Hereda de la clase abstracta/interfaz Game.
+    """
+
+    # Constantes
+    PLAYER_ONE_COLOR = "Blancas"
+    PLAYER_TWO_COLOR = "Negras"
 
     def __init__(self, player_one, player_two, time_per_player):
         self.player_one = player_one
@@ -10,9 +18,13 @@ class ChessGame(Game):
         self.name_base = os.path.basename(self.name_file)
         self.time_per_player = time_per_player
 
-
     def start(self):
+        print("-" * 50)
+        print(f" Juego de Ajedrez Iniciado!")
+        print(f"Participantes: {self.player_one} ({self.PLAYER_ONE_COLOR}) vs {self.player_two} ({self.PLAYER_TWO_COLOR}) ")
+        print(f"Tiempo por jugador: {self.time_per_player} minutos.")
         print(f"[{self.name_base}] Players {self.player_one} and {self.player_two} start chess game")
         print(f"Chess match started between {self.player_one}(White) and {self.player_two}(Black)")
+        print("-" * 50)
 
     """ Concrete product: ChessGame"""
