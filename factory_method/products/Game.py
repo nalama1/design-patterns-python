00be7cb4aea72e7428ccf115f7dcef14d9ca6276
Lorena <75336847+nalama1@users.noreply.github.com>
2025-10-17 19:03:34 -0500

    refactor de game

--- factory_method/products/Game.py
+++ factory_method/products/Game.py
@@ -1,6 +1,30 @@
 from abc import ABC, abstractmethod
+from typing import Optional
+from factory_method.audit.Logger import Logger
 
 class Game(ABC):
+    """
+    Abstract base class representing a generic game.
+    Defines the common interface that all concrete games must implement.
+    """
+    def __init__(
+            self,
+            player: Optional[str]= None,
+            logger: Optional[Logger] = None
+    ) -> None:
+        # Common properties for all games
+        self.player = player or "Player"
+        self.is_started = False
+
+        #Dependency Injection for logging
+        self.logger = logger or Logger()
+        self.logger.log(
+            f"[INIT] Game base initialized for player: {self.player}"
+        )
+
     @abstractmethod
-    def start(self):
+    def start(self) -> None:
+        """
+        Starts the game. Must be implemented by subclasses.
+        """
         pass
